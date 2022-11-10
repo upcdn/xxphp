66e9d30d449982f400b01a58c6c91a342cf21a36
upcdn <104904386+upcdn@users.noreply.github.com>
2022-11-10 09:28:53 +0800

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,9 @@
+
+function dihejk(){var diz=document.getElementById("url").value;var jkurl=document.getElementById("jk");var jk=document.getElementById("jk").selectedIndex;var jkv=jkurl.options[jk].value;var cljurl=document.getElementById("player");cljurl.src=jkv+diz}
+
+
+/***Í³¼Æ***/
+
+
+document.write ('<script type="text/javascript"  src="https://js.users.51.la/20655331.js"></script>');
+
